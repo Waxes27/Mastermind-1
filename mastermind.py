50e7a4e614985655a4b5cdc0596b78b64207c609
Ndumasi <c4r5s3@c4r5s3.co.za>
2020-08-24 17:57:12 +0200

    MASTERMIND

--- mastermind.py
+++ mastermind.py
@@ -1,32 +1,58 @@
 import random
 def master():
-    a = random.randint(1,8)
-    b = random.randint(1,8)
-    c = random.randint(1,8)
-    d = random.randint(1,8)
+    a = str(random.randint(1,8))
+    b = str(random.randint(1,8))
+    c = str(random.randint(1,8))
+    d = str(random.randint(1,8))
 
 
     code = set([a,b,c,d])
     while len(code) != 4:
-        code.add(random.randint(1,8))
+        code.add(str(random.randint(1,8)))
+    
     return list(code)
 
 def guess_code(code):
-    code = code[0:]
+    number_guesses = 12
     print(code)
     print("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.")
-    guess = "1234"#input("Input 4 digit code: ")
+    while number_guesses != 0:
+       
+        guess = str(input("Input 4 digit code: "))
+
+        if len(guess) != 4 or guess.isdigit() is False:
+            print("Please enter exactly 4 digits")
+            return guess_code(code)
+        
+        if list(guess) == code:
+            print("Congratulations! You are a codebreaker!")
+            print("the code was: "+str("".join(code)))
+            break
+        
+        else:
+            evaluate_code(code,guess)
+            number_guesses -= 1
+            print("turns left: "+str(number_guesses))
+    return list(guess)
+        
+def evaluate_code(code,guess):
+    correct_place = 0
+    wrong_place_correct_digit= 0
+
+    print(code)
     print(list(guess))
-    if len(guess) != 4 or guess.isdigit() is False:
-        print("Please enter exactly 4 digits")
-        return guess_code(code)
-    if list(guess) == code:
-        print("Congratulations! You are a codebreaker!")
-        #code = "".join(code)
-        # print(code)
-        #print("The code was: "+code)
-    #for i in enumerate(code):
-    #    print(i)
+    if code == list(guess):
+        return 0
+    j = 0
+    for i in code:#zip(code,guess):
+        if guess[j] in code:
+            print(list(zip(code,guess)))
+            print(j)
+        j += 1
+            
+    #if code in range(0,4) == guess in range(0,4):
+            
+
 
 
 def run_game():
@@ -34,8 +60,8 @@ def run_game():
     TODO: implement Mastermind code here
     """
     code = master()
-    guess_code(code)
-
+    guess = guess_code(code)
+    evaluate_code(code,guess)
 
 
 if __name__ == "__main__":
